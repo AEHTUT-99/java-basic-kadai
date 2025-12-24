a682e1dd572c2aee3874c9b01c90eb0542c146c1
AEHTUT-99 <aehtut999@gmail.com>
2025-12-24 23:30:05 +0900

    Dictionary_Chapter21.javaをアップロード

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,48 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+
+    
+    HashMap<String, String> dictionary = new HashMap<String, String>();
+
+    
+    public Dictionary_Chapter21() {
+        dictionary.put("apple", "りんご");
+        dictionary.put("peach", "桃");
+        dictionary.put("banana", "バナナ");
+        dictionary.put("lemon", "レモン");
+        dictionary.put("pear", "梨");
+        dictionary.put("kiwi", "キウィ");
+        dictionary.put("strawberry", "いちご");
+        dictionary.put("grape", "ぶどう");
+        dictionary.put("muscat", "マスカット");
+        dictionary.put("cherry", "さくらんぼ");
+    }
+
+    
+    public void searchWord(String word) {
+        boolean found = false;
+
+        
+        for (String key : dictionary.keySet()) {
+            if (key.equals(word)) {
+                System.out.println(
+                    word + "の意味は" + dictionary.get(key)
+                );
+                found = true;
+                break;
+            }
+        }
+
+        
+        if (!found) {
+            System.out.println(word + "は辞書に存在しません");
+        }
+    }
+}
+
+	 	
+		
+	
